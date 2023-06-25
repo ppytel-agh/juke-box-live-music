a4fa83751f043ddfb15b8aa3bd4094ddd443a95d
jacobwojcik <jkbwojcik11@gmail.com>
2023-06-25 14:02:17 +0200

    Adjust event form

--- app/frontend/src/components/EventForm/EventForm.tsx
+++ app/frontend/src/components/EventForm/EventForm.tsx
@@ -1,34 +1,45 @@
 import React, { useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
 import useToken from '../../hooks/useToken';
+import { useLocation } from 'react-router-dom';
 
 const EventForm = () => {
-  const [selectedValue, setSelectedValue] = useState('');
-  
-  const {getToken} = useToken()
+  const [selectedValue, setSelectedValue] = useState('1');
+  const [selectedType, setSelectedType] = useState('100');
 
-  const handleBuyTickets = async () => {
-    const res = await
-      fetch('http://localhost:8080/api/events', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'authorization': `${JSON.parse(getToken() ?? '')}`
-        },
-        body: selectedValue
-      })
-  };
+  const { getToken, token } = useToken();
+  const id = useLocation().pathname.split('/')[2];
 
-  const handleSubmit = (event: any) => {
+  const handleSubmit = async (event: any) => {
     event.preventDefault();
-    try{
-      // 
 
-      alert("Udało się kupić bilet!")
-      window.location.replace('/kupiono-bilet');
-    }
-    catch(err) {
-      alert(err)
+    try {
+      const userToken: any = JSON.parse(token ?? '');
+      const id_koncertu = id;
+      const liczba_biletow = selectedValue;
+      const koszt_calkowity = selectedType;
+
+      const requestBody = {
+        token: userToken,
+        id_koncertu,
+        liczba_biletow,
+        koszt_calkowity,
+      };
+
+      // TODO fix the request
+      // const res = await fetch('http://localhost:8080/api/add-new-booking', {
+      //   method: 'POST',
+      //   headers: {
+      //     'Content-Type': 'application/json',
+      //     authorization: `${JSON.parse(getToken() ?? '')}`,
+      //   },
+      //   body: JSON.stringify(requestBody),
+      // });
+      //
+      // alert('Udało się kupić bilet!');
+      // window.location.replace('/kupiono-bilet');
+    } catch (err) {
+      alert(err);
     }
   };
 
@@ -41,24 +52,32 @@ const EventForm = () => {
         paddingTop: '180px',
       }}
     >
-      <h2>Nazwa koncertu</h2>
+      <h2>Kup bilet!</h2>
       <Form>
-        <Form.Group controlId="imie">
-          <Form.Label className="mt-3">Imię</Form.Label>
-          <Form.Control type="text" placeholder="Wpisz swoje imię" />
-        </Form.Group>
-
-        <Form.Group controlId="nazwisko">
-          <Form.Label className="mt-3">Nazwisko</Form.Label>
-          <Form.Control type="text" placeholder="Wpisz swoje nazwisko" />
-        </Form.Group>
-
         <Form.Group controlId="iloscBiletow">
           <Form.Label className="mt-3">Ilość biletów</Form.Label>
-          <Form.Control as="select" value={selectedValue} onChange={e => setSelectedValue(e.target.value)}>
+          <Form.Control
+            as="select"
+            value={selectedValue}
+            onChange={(e) => setSelectedValue(e.target.value)}
+          >
             {[...Array(10)].map((_, index) => (
-              <option key={index + 1} value={index + 1}>{index + 1}</option>
+              <option key={index + 1} value={index + 1}>
+                {index + 1}
+              </option>
             ))}
+          </Form.Control>
+        </Form.Group>
+        <Form.Group controlId="iloscBiletow">
+          <Form.Label className="mt-3">Rodzaj</Form.Label>
+          <Form.Control
+            as="select"
+            value={selectedType}
+            onChange={(e) => setSelectedType(e.target.value)}
+          >
+            <option value="100">Standard - 100 zł</option>
+            <option value="200">Premium - 200 zł</option>
+            <option value="300">VIP - 300 zł</option>
           </Form.Control>
         </Form.Group>
 
