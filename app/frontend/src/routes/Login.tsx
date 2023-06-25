88daee1b9b9b6c4b801f3f30343178e297a7ec82
jacobwojcik <jkbwojcik11@gmail.com>
2023-06-25 13:00:35 +0200

    Fix login and sign up

--- app/frontend/src/routes/Login.tsx
+++ app/frontend/src/routes/Login.tsx
@@ -3,7 +3,7 @@ import { Form, Button } from 'react-bootstrap';
 import Header from '../components/Header/Header';
 
 const LoginPage = () => {
-  const [loginData, setLoginData] = useState({ email: '', haslo: '' })
+  const [loginData, setLoginData] = useState({ haslo: '' });
 
   function setToken(userToken: any) {
     sessionStorage.setItem('token', JSON.stringify(userToken));
@@ -11,7 +11,7 @@ const LoginPage = () => {
 
   function getToken() {
     const tokenString = sessionStorage.getItem('token');
-    return tokenString
+    return tokenString;
   }
 
   const onFormChange = (event: ChangeEvent<HTMLInputElement>) => {
@@ -22,72 +22,75 @@ const LoginPage = () => {
     setLoginData({ ...loginData, [name]: value });
   };
 
-  async function loginUser(loginData: { email: string, haslo: string }) {
+  async function loginUser(loginData: { haslo: string }) {
     return fetch('http://localhost:8080/login', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body: JSON.stringify(loginData)
-    })
-      .then(data => data)
-   }
+      body: JSON.stringify(loginData),
+    }).then((data) => data);
+  }
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
-    try{
+    try {
       const token = await loginUser(loginData);
-      const tokenValue = await token.json()
-      
+      if (token.status !== 200) {
+        const err = await token.text();
+        throw new Error(err);
+      }
+      const tokenValue = await token.json();
+
       setToken(tokenValue.token);
-      alert("Udało się zalogować!")
-      window.location.replace('/')
+      alert('Udało się zalogować!');
+      window.location.replace('/');
+    } catch (err: any) {
+      const errorMessage = err.message.replace('Error: ', '');
+      alert(errorMessage);
     }
-    catch(err) {
-      alert(err)
-    }
-  }
+  };
 
   return (
     <>
-    <Header />
-    <div
-      className="d-flex align-items-center justify-content-center vh-100"
-      style={{ backgroundColor: '#BFD2FF' }}
-    >
-      <div className="text-center">
-        <h1>Logowanie</h1>
-        <Form>
-          <Form.Group
-            controlId="formBasicEmail"
-            className="d-flex flex-column align-items-start my-3"
-          >
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" name='email' onChange={onFormChange}/>
-          </Form.Group>
+      <Header />
+      <div
+        className="d-flex align-items-center justify-content-center vh-100"
+        style={{ backgroundColor: '#BFD2FF' }}
+      >
+        <div className="text-center">
+          <h1>Logowanie</h1>
+          <Form>
+            <Form.Group
+              controlId="formBasicUsername"
+              className="d-flex flex-column align-items-start my-3"
+            >
+              <Form.Label>Nazwa użytkownika</Form.Label>
+              <Form.Control
+                type="email"
+                name="nazwa_uzytkownika"
+                onChange={onFormChange}
+              />
+            </Form.Group>
 
-          <Form.Group
-            controlId="formBasicUsername"
-            className="d-flex flex-column align-items-start my-3"
-          >
-            <Form.Label>nazwa_uzytkownika</Form.Label>
-            <Form.Control type="email" name='nazwa_uzytkownika' onChange={onFormChange}/>
-          </Form.Group>
+            <Form.Group
+              controlId="formBasicPassword"
+              className="d-flex flex-column align-items-start my-3"
+            >
+              <Form.Label>Hasło</Form.Label>
+              <Form.Control
+                type="password"
+                name="haslo"
+                onChange={onFormChange}
+              />
+            </Form.Group>
 
-          <Form.Group
-            controlId="formBasicPassword"
-            className="d-flex flex-column align-items-start my-3"
-          >
-            <Form.Label>Hasło</Form.Label>
-            <Form.Control type="password" name='haslo' onChange={onFormChange}/>
-          </Form.Group>
-
-          <Button variant="primary" type="submit"  onClick={handleSubmit}>
-            Zaloguj się
-          </Button>
-        </Form>
+            <Button variant="primary" type="submit" onClick={handleSubmit}>
+              Zaloguj się
+            </Button>
+          </Form>
+        </div>
       </div>
-    </div>
     </>
   );
 };
--- app/server/src/routes/register-new-user.js
+++ app/server/src/routes/register-new-user.js
@@ -7,15 +7,15 @@ module.exports = async (req, res) => {
     const connection = typeorm.getConnection();
     const userRepository = connection.getRepository(User);
 
-    // const existingUser = await userRepository.findOne({ where: [{ nazwa_uzytkownika: req.body.nazwa_uzytkownika }, { email: req.body.email }] });
-    // if (existingUser) {
-    //   return res.status(400).send("Username or e-mail already in use");
-    // }
+    const existingUser = await userRepository.findOne({ where: [{ nazwa_uzytkownika: req.body.nazwa_uzytkownika }, { email: req.body.email }] });
+    if (existingUser) {
+      return res.status(400).send("Użytkownik o podanym loginie lub emailu istnieje!");
+    }
 
-    // const passwordRegex = /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).{8,}/;
-    // if (!passwordRegex.test(req.body.haslo)) {
-    //   return res.status(400).send("The password must contain a number, a capital letter and a special character");
-    // }
+    const passwordRegex = /(?=.*[A-Z])(?=.*[!@#$&*])(?=.*[0-9]).{8,}/;
+    if (!passwordRegex.test(req.body.haslo)) {
+      return res.status(400).send("Twoje hasło musi zawierać co najmniej jedną wielką literę, jeden znak specjalny, jedną cyfrę i mieć co najmniej 8 znaków długości.");
+    }
 
     const hashedPassword = await bcrypt.hash(req.body.haslo, 10);
 
@@ -24,7 +24,7 @@ module.exports = async (req, res) => {
 
     await userRepository.save(user);
 
-    res.status(200).send("User created successfully");
+    res.status(200).send("Udało się zarejestrować!");
   } catch (error) {
     console.log('Error occurred:', error);
     res.status(500).send(`An error occurred: ${error.message}`);
